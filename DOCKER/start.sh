65a6a6b62fa8594aaa2c3bdf44646497d1d705ff
AlexisMalattia <alexis.malattia06@gmail.com>
2023-12-04 16:49:37 +0100

    Ajout du connecteur python pour hdfs

--- DOCKER/start.sh
+++ DOCKER/start.sh
@@ -3,3 +3,6 @@ docker-compose up -d
 
 docker exec -it tpa-mongo sh ./tpa-mongo/initDB.sh
 docker exec -it tpa-postgres sh ./tpa-postgres/importPostgresDatasource.sh
+docker cp ./hadoop-spark-kafka/volumes/datasource/CO2.csv datanode:/usr
+docker exec -it datanode bash
+hdfs dfs -test -e /user/fichiers/CO2.csv || { hdfs dfs -mkdir -p /user/fichiers && hdfs dfs -put /usr/CO2.csv /user/fichiers; }
