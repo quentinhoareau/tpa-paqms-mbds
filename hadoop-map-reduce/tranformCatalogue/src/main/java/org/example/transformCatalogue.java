36f6ca5f7ff3cf99c0ad91195da40b43bdd5b0e7
Sylvain <37038164+syl9456@users.noreply.github.com>
2023-12-27 00:18:27 +0100

    ajustements

--- hadoop-map-reduce/tranformCatalogue/src/main/java/org/example/transformCatalogue.java
+++ hadoop-map-reduce/tranformCatalogue/src/main/java/org/example/transformCatalogue.java
@@ -6,9 +6,9 @@ import java.util.Map;
 public class transformCatalogue {
 
     public static void main(String[] args) throws Exception {
-        String hdfsUri = "hdfs:///";
-        String hdfsFilePath = "/results/part*";
-        String couchDbUrl = "http://127.0.0.1:5984/tpa/";
+        String hdfsUri = "hdfs://namenode:8020";
+        String hdfsFilePath = "results/part-r-00000";
+        String couchDbUrl = "http://admin:password@127.0.0.1:5984/tpa/";
 
         Map<String, String[]> hdfsData = HDFSReader.readHdfsFile(hdfsUri, hdfsFilePath);
         CouchDBUpdater.updateCouchDBDocuments(couchDbUrl, hdfsData);
