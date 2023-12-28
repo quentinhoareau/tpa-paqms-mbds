64cf78dbb08c7e4b479b36c0d1ccc1030f8bf7f8
Sylvain <37038164+syl9456@users.noreply.github.com>
2023-12-28 17:37:49 +0100

    Fin MapReduce

--- DOCKER/tpa-datanode/runMapReduce.sh
+++ DOCKER/tpa-datanode/runMapReduce.sh
@@ -1,11 +1,7 @@
 echo "removing results"
-hdfs dfs -ls .
-hdfs dfs -rm -r results
-
+hdfs dfs -ls /user/fichiers/
+hdfs dfs -rm -r /user/fichiers/results
 echo "runing CO2 analysis"
-hadoop jar usr/hadoop-map-reduce.jar /user/fichiers/cleaned_CO2.csv results
+hadoop jar usr/hadoop-map-reduce.jar /user/fichiers/cleaned_CO2.csv /user/fichiers/results
 echo "CO2 analysis done"
-hdfs dfs -cat results/*
-echo "runing Catalogue transformation on CouchDB"
-hadoop jar usr/transformCatalogue.jar
-echo "Catalogue transformation done"
+hdfs dfs -cat /user/fichiers/results/*
